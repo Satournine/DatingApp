2239aece53c4a1493a3d29a10f73106fc6509b84
Satournine <tunaorkun@outlook.com>
2023-11-25 21:48:36 +0300

    Auth

--- DatingAppAPI/Entities/AppUser.cs
+++ DatingAppAPI/Entities/AppUser.cs
@@ -1,8 +1,11 @@
-﻿namespace DatingAppAPI.Entities
+﻿
+namespace DatingAppAPI.Entities
 {
     public class AppUser
     {
         public int Id { get; set; }
         public string UserName { get; set; }
+        public byte[] PasswordHash { get; set;}
+        public byte[] PasswordSalt { get; set; }
     }
 }
